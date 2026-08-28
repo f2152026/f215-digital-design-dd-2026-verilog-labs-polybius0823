ce414553d1920861f3def436476acfb77374856d
Aishik Biswas <f20250823@pilani.bits-pilani.ac.in>
2026-08-28 11:47:09 +0000

    lab01 task4

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -24,20 +24,7 @@ module FA_Gate(
   //xor #(2) (ps,  a,   b);
   or  #(2) (cout, pc1, pc2);
 
-  // or (cout, pc1, pc2);
-  // //xor (ps,  a,   b);
-  // and (pc1, a,   b);
-  // xor (sum, cin, ps);
-  // and (pc2, cin, ps);
-  // xor (ps,  a,   b);
-  // //or  (cout, pc1, pc2);
-
-  // //xor (ps,  a,   b);
-  // xor (ps,  a,   b);
-  // and (pc1, a,   b);
-  // xor (sum, cin, ps);
-  // and (pc2, cin, ps);
-  // or (cout, pc1, pc2);
-  // //or  (cout, pc1, pc2);
+  // 1b ans : changing the order doesn't change the waveform as all the gates are executed simultaneously, sum and cout give the same answer  
+  // 1c ans : with delays the waveform does change as each output depends on the cascading delays from gates before it, sum and cout give outputs later
 
 endmodule
